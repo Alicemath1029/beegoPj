a9b222252b6856dc4f949f7d8f825666a5eb1622
Alicemath1029 <327034486@qq.com>
2023-04-08 20:15:43 +0800

    bbbbb

--- happy/controllers/default.go
+++ happy/controllers/default.go
@@ -9,7 +9,7 @@ type MainController struct {
 }
 
 func (c *MainController) Get() {
-	c.Data["Website"] = "www.baidu.com"
+	c.Data["Website"] = "shop.miaow.com"
 	c.Data["Email"] = "astaxie@gmail.com"
 	c.TplName = "index.tpl"
 }
